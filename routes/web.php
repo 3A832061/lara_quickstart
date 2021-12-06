bae87adec4212362eda353f827c087f49f3f3f55
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-12-06 20:12:14 +0800

    建立顯示、新增、刪除三個路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+});
+Route::post('/task', function (Request $request) {
+
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+
 });
